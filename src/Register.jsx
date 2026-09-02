736b0b41664f9bff71164eddc9e67618ff1487be
Amjad Ali <amjadali@Amjads-MacBook-Pro.local>
2026-09-02 16:59:57 +0530

    Initial commit 4

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import usersData from "./Users.json";
+import usersData from "./users.json";
 
 function Register() {
   const [formData, setFormData] = useState({
